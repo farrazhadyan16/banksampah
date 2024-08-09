68c46c15a6bc281d8af53519cfede83cbdb03ba3
Mardhyah <mardhyahfathaniaizzati9d@gmail.com>
2024-08-09 14:45:58 +0700

    get sampah

--- fungsi.php
+++ fungsi.php
@@ -14,11 +14,12 @@ function query($query)
 }
 
 
-function inputdata($data){
+function inputdata($data)
+{
     global $conn;
-    $id= htmlspecialchars($data["id"]);
-    $no_jo = htmlspecialchars($data["no_jo"]);
-    $tgl_jo = htmlspecialchars($data["tgl_jo"]);
+    $id = htmlspecialchars($data["id"]);
+    $id_kategori = htmlspecialchars($data["id_kategori"]);
+    $jenis = htmlspecialchars($data["jenis"]);
     $nama_project = htmlspecialchars($data["nama_project"]);
     $kode_gbj = htmlspecialchars($data["kode_gbj"]);
     $nilai_harga = htmlspecialchars($data["nilai_harga"]);
@@ -57,14 +58,14 @@ function inputdata($data){
     $delivery_tgl = htmlspecialchars($data["delivery_tgl"]);
     $keterangan = htmlspecialchars($data["keterangan"]);
 
-    $query = "INSERT INTO data_monitoring
-    (id,no_jo,tgl_jo,nama_project,kode_gbj,nilai_harga,nama_panel,tipe_jenis,tipe_fswm,qty_unit,qty_cell
+    $query = "INSERT INTO sampah
+    (id,id_kategori,jenis,nama_project,kode_gbj,nilai_harga,nama_panel,tipe_jenis,tipe_fswm,qty_unit,qty_cell
     ,nomor_wo,nomor_seri,warna,size_panel_height,size_panel_width,size_panel_deep,mh_std,mh_aktual,
     tgl_submit_dwg_for_approval, tgl_approved,tgl_release_dwg_softcopy,tgl_release_dwg_hardcopy,breakdown,busbar,
     target_ppc,target_eng,tgl_box_selesai,due_date,tgl_terbit_wo,plan_start_produksi,aktual_start_produksi,
     plan_fg_wo,progress,desc_progress,status,delivery_no,delivery_tgl,keterangan)
     VALUES
-    ('$id','$no_jo','$tgl_jo','$nama_project','$kode_gbj','$nilai_harga','$nama_panel','$tipe_jenis',
+    ('$id','$id_kategori','$jenis','$nama_project','$kode_gbj','$nilai_harga','$nama_panel','$tipe_jenis',
     '$tipe_fswm','$qty_unit','$qty_cell','$warna','$nomor_wo','$nomor_seri','$size_panel_height',
     '$size_panel_width','$size_panel_deep','$mh_std','$mh_aktual','$tgl_submit_dwg_for_approval','$tgl_approved',
     '$tgl_release_dwg_softcopy','$tgl_release_dwg_hardcopy','$breakdown','$busbar','$targetppcleadtime',
@@ -72,14 +73,13 @@ function inputdata($data){
     '$plan_fg_wo','$progress','$desc_progress','$status','$delivery_no','$delivery_tgl','$keterangan')";
     mysqli_query($conn, $query);
     return mysqli_affected_rows($conn);
-
 }
 
 function getProjectIds()
 {
     global $conn;
 
-    $query = "SELECT id FROM data_monitoring"; // Replace 'your_project_table' with the actual table name
+    $query = "SELECT id FROM sampah"; // Replace 'your_project_table' with the actual table name
     $result = mysqli_query($conn, $query);
 
     $projectIds = [];
@@ -91,11 +91,12 @@ function getProjectIds()
 }
 
 
-function updatedatamonitoring($data){
+function updatedatamonitoring($data)
+{
     global $conn;
-    $id= htmlspecialchars($data["id"]);
-    $no_jo = htmlspecialchars($data["no_jo"]);
-    $tgl_jo = htmlspecialchars($data["tgl_jo"]);
+    $id = htmlspecialchars($data["id"]);
+    $id_kategori = htmlspecialchars($data["id_kategori"]);
+    $jenis = htmlspecialchars($data["jenis"]);
     $nama_project = htmlspecialchars($data["nama_project"]);
     $kode_gbj = htmlspecialchars($data["kode_gbj"]);
     $nilai_harga = htmlspecialchars($data["nilai_harga"]);
@@ -134,69 +135,75 @@ function updatedatamonitoring($data){
     $delivery_tgl = htmlspecialchars($data["delivery_tgl"]);
     $keterangan = htmlspecialchars($data["keterangan"]);
 
-    $query = "UPDATE data_monitoring SET no_jo='$no_jo',tgl_jo='$tgl_jo',nama_project='$nama_project',kode_gbj='$kode_gbj',nilai_harga='$nilai_harga',nama_panel='$nama_panel',tipe_jenis='$tipe_jenis',tipe_fswm='$tipe_fswm',qty_unit='$qty_unit',qty_cell='$qty_cell',nomor_wo='$nomor_wo',nomor_seri='$nomor_seri',warna='$warna',size_panel_height='$size_panel_height',size_panel_width='$size_panel_width',size_panel_deep='$size_panel_deep',mh_std='$mh_std',mh_aktual='$mh_aktual',tgl_submit_dwg_for_approval='$tgl_submit_dwg_for_approval', tgl_approved='$tgl_approved',tgl_release_dwg_softcopy='$tgl_release_dwg_softcopy',tgl_release_dwg_hardcopy='$tgl_release_dwg_hardcopy',breakdown='$breakdown',busbar='$busbar',target_ppc='$targetppcleadtime',target_eng='$targetengleadtime',tgl_box_selesai='$tgl_box_selesai',due_date='$due_date',tgl_terbit_wo='$tgl_terbit_wo',plan_start_produksi='$plan_start_produksi',aktual_start_produksi='$aktual_start_produksi',plan_fg_wo='$plan_fg_wo',progress='$progress',desc_progress='$desc_progress',status='$status',delivery_no='$delivery_no',delivery_tgl='$delivery_tgl',keterangan='$keterangan' WHERE id='$id'";
+    $query = "UPDATE sampah SET id_kategori='$id_kategori',jenis='$jenis',nama_project='$nama_project',kode_gbj='$kode_gbj',nilai_harga='$nilai_harga',nama_panel='$nama_panel',tipe_jenis='$tipe_jenis',tipe_fswm='$tipe_fswm',qty_unit='$qty_unit',qty_cell='$qty_cell',nomor_wo='$nomor_wo',nomor_seri='$nomor_seri',warna='$warna',size_panel_height='$size_panel_height',size_panel_width='$size_panel_width',size_panel_deep='$size_panel_deep',mh_std='$mh_std',mh_aktual='$mh_aktual',tgl_submit_dwg_for_approval='$tgl_submit_dwg_for_approval', tgl_approved='$tgl_approved',tgl_release_dwg_softcopy='$tgl_release_dwg_softcopy',tgl_release_dwg_hardcopy='$tgl_release_dwg_hardcopy',breakdown='$breakdown',busbar='$busbar',target_ppc='$targetppcleadtime',target_eng='$targetengleadtime',tgl_box_selesai='$tgl_box_selesai',due_date='$due_date',tgl_terbit_wo='$tgl_terbit_wo',plan_start_produksi='$plan_start_produksi',aktual_start_produksi='$aktual_start_produksi',plan_fg_wo='$plan_fg_wo',progress='$progress',desc_progress='$desc_progress',status='$status',delivery_no='$delivery_no',delivery_tgl='$delivery_tgl',keterangan='$keterangan' WHERE id='$id'";
     mysqli_query($conn, $query);
     return mysqli_affected_rows($conn);
 }
 
 
 
-function hapusMonitoring($id){
+function hapusMonitoring($id)
+{
     global $conn;
-    mysqli_query($conn, "DELETE FROM data_monitoring WHERE id = '$id'");
+    mysqli_query($conn, "DELETE FROM sampah WHERE id = '$id'");
     return mysqli_affected_rows($conn);
 }
-function updateDesign($data) {
+function updateDesign($data)
+{
     global $conn;
     $id = htmlspecialchars($data["id"]);
     $designpic = htmlspecialchars($data["design_pic"]);
     $designstart = htmlspecialchars($data["design_start"]);
     $designend = htmlspecialchars($data["design_end"]);
 
-    $query = "UPDATE data_monitoring SET design_pic='$designpic', design_start='$designstart', design_end='$designend' WHERE id='$id'";
+    $query = "UPDATE sampah SET design_pic='$designpic', design_start='$designstart', design_end='$designend' WHERE id='$id'";
     mysqli_query($conn, $query);
     return mysqli_affected_rows($conn);
 }
 
-function updateNesting($data) {
+function updateNesting($data)
+{
     global $conn;
     $id = htmlspecialchars($data["id"]);
     $nestingpic = htmlspecialchars($data["nesting_pic"]);
     $nestingstart = htmlspecialchars($data["nesting_start"]);
     $nestingend = htmlspecialchars($data["nesting_end"]);
 
-    $query = "UPDATE data_monitoring SET nesting_pic='$nestingpic', nesting_start='$nestingstart', nesting_end='$nestingend' WHERE id='$id'";
+    $query = "UPDATE sampah SET nesting_pic='$nestingpic', nesting_start='$nestingstart', nesting_end='$nestingend' WHERE id='$id'";
     mysqli_query($conn, $query);
     return mysqli_affected_rows($conn);
 }
 
-function updateProgram($data) {
+function updateProgram($data)
+{
     global $conn;
     $id = htmlspecialchars($data["id"]);
     $programpic = htmlspecialchars($data["program_pic"]);
     $programstart = htmlspecialchars($data["program_start"]);
     $programend = htmlspecialchars($data["program_end"]);
 
-    $query = "UPDATE data_monitoring SET program_pic='$programpic', program_start='$programstart', program_end='$programend' WHERE id='$id'";
+    $query = "UPDATE sampah SET program_pic='$programpic', program_start='$programstart', program_end='$programend' WHERE id='$id'";
     mysqli_query($conn, $query);
     return mysqli_affected_rows($conn);
 }
 
-function updateChecker($data) {
+function updateChecker($data)
+{
     global $conn;
     $id = htmlspecialchars($data["id"]);
     $checkerpic = htmlspecialchars($data["checker_pic"]);
     $checkerstart = htmlspecialchars($data["checker_start"]);
     $checkerend = htmlspecialchars($data["checker_end"]);
 
-    $query = "UPDATE data_monitoring SET checker_pic='$checkerpic', checker_start='$checkerstart', checker_end='$checkerend' WHERE id='$id'";
+    $query = "UPDATE sampah SET checker_pic='$checkerpic', checker_start='$checkerstart', checker_end='$checkerend' WHERE id='$id'";
     mysqli_query($conn, $query);
     return mysqli_affected_rows($conn);
 }
 
-function inputdatakonsesi($data){
+function inputdatakonsesi($data)
+{
     global $conn;
-    $idkonsesi= htmlspecialchars($data["id_konsesi"]);
+    $idkonsesi = htmlspecialchars($data["id_konsesi"]);
     $jo = htmlspecialchars($data["jo"]);
     $wo = htmlspecialchars($data["wo"]);
     $nameproject = htmlspecialchars($data["nama_project"]);
@@ -221,7 +228,6 @@ function inputdatakonsesi($data){
                 '$nolkpj', '$status', '$tglmatrial', '$tglpasang', '$keterangan')";
     mysqli_query($conn, $query);
     return mysqli_affected_rows($conn);
-
 }
 
 // Function Baca Data Konsesi
@@ -241,9 +247,10 @@ function hapusKonsesi($id)
     return mysqli_affected_rows($conn);
 }
 
-function updatedatakonsesi($data){
+function updatedatakonsesi($data)
+{
     global $conn;
-    $id= $data["id_konsesi"];
+    $id = $data["id_konsesi"];
     $jo = htmlspecialchars($data["jo"]);
     $wo = htmlspecialchars($data["wo"]);
     $nameproject = htmlspecialchars($data["nama_project"]);
@@ -261,8 +268,4 @@ function updatedatakonsesi($data){
     $query = "UPDATE konsesi SET jo='$jo',wo='$wo',nama_project='$nameproject',nama_panel='$namepanel',unit='$unit',jenis='$jenis',konsesi='$konsesi',jumlah='$jumlah',no_lkpj='$nolkpj',status='$status',tgl_matrial_dtg='$tglmatrial',tgl_pasang='$tglpasang',keterangan='$keterangan' WHERE id_konsesi='$id'";
     mysqli_query($conn, $query);
     return mysqli_affected_rows($conn);
-
 }
-
-
-?>
--- monitoring.php
+++ monitoring.php
@@ -2,7 +2,7 @@
 include 'header.php';
 include 'fungsi.php';
 
-$query_all = query("SELECT * from data_monitoring ORDER BY LENGTH(id),CAST(id AS UNSIGNED)");
+$query_all = query("SELECT * from sampah ORDER BY LENGTH(id),CAST(id AS UNSIGNED)");
 
 ?>
 
@@ -60,8 +60,8 @@ $query_all = query("SELECT * from data_monitoring ORDER BY LENGTH(id),CAST(id AS
             <div class="tabular--wrapper">
                 <h3 class="main--title">Data Project</h3>
                 <?php
-                if(isset($_SESSION['message'])){
-                    echo "<h4>".$_SESSION['message']."</h4>";
+                if (isset($_SESSION['message'])) {
+                    echo "<h4>" . $_SESSION['message'] . "</h4>";
                     unset($_SESSION['message']);
                 }
                 ?>
@@ -69,58 +69,12 @@ $query_all = query("SELECT * from data_monitoring ORDER BY LENGTH(id),CAST(id AS
                     <table>
                         <thead>
                             <tr>
-                                <th>ID PROJECT</th>
-                                <th>NO JO</th>
-                                <th>TGL JO</th>
-                                <th>PROYEK</th>
-                                <th>KODE GBJ</th>
-                                <th>NILAI HARGA</th>
-                                <th>PANEL NAME</th>
-                                <th>TYPE JENIS</th>
-                                <th>TYPE FS/WM</th>
-                                <th>QTY UNIT</th>
-                                <th>QTY CELL</th>
-                                <th>WARNA</th>
-                                <th>NOMOR WO</th>
-                                <th>NO.SERI</th>
-                                <th>HEIGTH</th>
-                                <th>WIDTH</th>
-                                <th>DEEP</th>
-                                <th>MH STD</th>
-                                <th>MH AKTUAL</th>
-                                <th>TGL.SUBMIT DWG FOR APPROVAL</th>
-                                <th>TGL APPROVED</th>
-                                <th>TGL RELEASE DWG SOFT COPY</th>
-                                <th>TGL RELEASE DWG HARD COPY</th>
-                                <th>BREAKDOWN</th>
-                                <th>BUSBAR</th>
-                                <th>TARGET PPC</th>
-                                <th>TARGET ENG</th>
-                                <th>DESIGN PIC</th>
-                                <th>DESIGN START</th>
-                                <th>DESIGN END</th>
-                                <th>NESTING PIC </th>
-                                <th>NESTING START</th>
-                                <th>NESTING END</th>
-                                <th>PROGRAM PIC</th>
-                                <th>PROGRAM START</th>
-                                <th>PROGRAM END</th>
-                                <th>CHECKER PIC</th>
-                                <th>CHECKER START</th>
-                                <th>CHECKER END</th>
-                                <th>TGL BOX SELESAI</th>
-                                <th>KESEPAKATAN DGN CUST DUE DATE</th>
-                                <th>TGL TERBIT WO</th>
-                                <th>PLAN START PRODUKSI</th>
-                                <th>AKTUAL START PRODUKSI</th>
-                                <th>PLAN FG WO</th>
-                                <th>AKTUAL FG</th>
-                                <th>PROGRES</th>
-                                <th>DESKRIPSI PROGRES</th>
-                                <th>STATUS</th>
-                                <th>DELIVERI NO</th>
-                                <th>DELIVERI TGL</th>
-                                <th>KETERANGAN</th>
+                                <th>#</th>
+                                <th>kategori</th>
+                                <th>Jenis</th>
+                                <th>Harga Pengepul</th>
+                                <th>Harga Nasabah</th>
+                                <th>Jumalah (KG)</th>
                                 <th>AKSI</th>
                             </tr>
                         </thead>
@@ -128,177 +82,40 @@ $query_all = query("SELECT * from data_monitoring ORDER BY LENGTH(id),CAST(id AS
                             <?php $i = 1; ?>
                             <?php foreach ($query_all as $row): ?>
 
-                            <tr>
-                                <td>
-                                    <?= $row["id"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["no_jo"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["tgl_jo"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["nama_project"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["kode_gbj"]; ?>
-                                </td>
-                                <td>
-                                    Rp. <?= $row["nilai_harga"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["nama_panel"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["tipe_jenis"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["tipe_fswm"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["qty_unit"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["qty_cell"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["warna"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["nomor_wo"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["nomor_seri"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["size_panel_height"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["size_panel_width"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["size_panel_deep"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["mh_std"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["mh_aktual"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["tgl_submit_dwg_for_approval"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["tgl_approved"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["tgl_release_dwg_softcopy"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["tgl_release_dwg_hardcopy"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["breakdown"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["busbar"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["target_ppc"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["target_eng"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["design_pic"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["design_start"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["design_end"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["nesting_pic"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["nesting_start"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["nesting_end"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["program_pic"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["program_start"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["program_end"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["checker_pic"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["checker_start"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["checker_end"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["tgl_box_selesai"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["due_date"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["tgl_terbit_wo"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["plan_start_produksi"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["aktual_start_produksi"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["plan_fg_wo"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["aktual_fg_wo"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["progress"]; ?>%
-                                </td>
-                                <td>
-                                    <?= $row["desc_progress"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["status"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["delivery_no"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["delivery_tgl"]; ?>
-                                </td>
-                                <td>
-                                    <?= $row["keterangan"]; ?>
-                                </td>
-                                <td>
-                                    <li class="liaksi">
-                                        <button type="submit" name="submit"><a
-                                                href="editmonitoring.php?id=<?= $row["id"]; ?>"
-                                                class="inputbtn2">Ubah</a></button>
-                                    </li>
-                                    <li class="liaksi">
-                                        <button type="submit" name="submit"><a
-                                                href="hapusmonitoring.php?id=<?= $row["id"]; ?>"
-                                                class="inputbtn3">Hapus</a></button>
-                                    </li>
-                                </td>
-                            </tr>
-                            <?php $i++; ?>
+                                <tr>
+                                    <td>
+                                        <?= $row["id"]; ?>
+                                    </td>
+                                    <td>
+                                        <?= $row["jenis"]; ?>
+                                    </td>
+                                    <td>
+                                        <?= $row["jenis"]; ?>
+                                    </td>
+                                    <td>
+                                        <?= $row["harga"]; ?>
+                                    </td>
+                                    <td>
+                                        <?= $row["harga_pusat"]; ?>
+                                    </td>
+                                    <td>
+                                        Rp. <?= $row["jumlah"]; ?>
+                                    </td>
+
+                                    <td>
+                                        <li class="liaksi">
+                                            <button type="submit" name="submit"><a
+                                                    href="editmonitoring.php?id=<?= $row["id"]; ?>"
+                                                    class="inputbtn2">Ubah</a></button>
+                                        </li>
+                                        <li class="liaksi">
+                                            <button type="submit" name="submit"><a
+                                                    href="hapusmonitoring.php?id=<?= $row["id"]; ?>"
+                                                    class="inputbtn3">Hapus</a></button>
+                                        </li>
+                                    </td>
+                                </tr>
+                                <?php $i++; ?>
                             <?php endforeach; ?>
                         </tbody>
                         <tfoot>
