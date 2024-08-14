a6250e3f1ad159ddaaff41730fedce48429131d8
Farraz Hadyan <farrazhadyan16@gmail.com>
2024-08-14 15:14:57 +0700

    sidebar dan disable time

--- sidebar.php
+++ sidebar.php
@@ -8,6 +8,33 @@
     <link rel="stylesheet" href="./css/style.css">
     <!-- Font Awesome CDN for icons -->
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    <style>
+    .menu ul {
+        list-style-type: none;
+        padding-left: 20px;
+    }
+
+    .menu ul li {
+        padding: 8px 0;
+    }
+
+    .dropdown-content {
+        display: none;
+        list-style-type: none;
+        padding-left: 20px;
+    }
+
+    .dropdown:hover .dropdown-content {
+        display: block;
+    }
+
+    .dropdown>a:after {
+        content: '\f0d7';
+        font-family: "Font Awesome 5 Free";
+        font-weight: 900;
+        float: right;
+    }
+    </style>
 </head>
 
 <body>
@@ -28,11 +55,16 @@
                             <span>Sampah</span>
                         </a>
                     </li>
-                    <li>
-                        <a href="transaksi.php">
+                    <li class="dropdown">
+                        <a href="#">
                             <i class="fa-solid fa-exchange-alt"></i>
                             <span>Transaksi</span>
                         </a>
+                        <ul class="dropdown-content">
+                            <li><a href="setor_sampah.php">Setor Sampah</a></li>
+                            <li><a href="konversi_saldo.php">Konversi Saldo</a></li>
+                            <li><a href="tarik_saldo.php">Tarik Saldo</a></li>
+                        </ul>
                     </li>
                     <li>
                         <a href="admin.php">
