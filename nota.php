ad0b6bb974c38a6ad92234a3bebc8fd927cfc2e3
Mardhyah <mardhyahfathaniaizzati9d@gmail.com>
2024-08-16 16:08:15 +0700

    add colom kategori di nota

--- nota.php
+++ nota.php
@@ -13,7 +13,10 @@ if ($stmt = $conn->prepare($transaksi_query)) {
     $transaksi = $result->fetch_assoc();
 
     // Fetch details for items
-    $items_query = "SELECT * FROM transaksi_tb WHERE id_trans = ?";
+    $items_query = "SELECT t.*, k.name as kategori_name 
+                    FROM transaksi_tb t 
+                    JOIN kategori_sampah k ON t.kategori_id = k.id 
+                    WHERE t.id_trans = ?";
     $items_stmt = $conn->prepare($items_query);
     $items_stmt->bind_param("s", $id_trans);
     $items_stmt->execute();
@@ -37,15 +40,15 @@ if ($stmt = $conn->prepare($transaksi_query)) {
     <title>Nota</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
-    .print-button {
-        margin-left: 10px;
-    }
-
-    @media print {
-        .no-print {
-            display: none;
+        .print-button {
+            margin-left: 10px;
         }
-    }
+
+        @media print {
+            .no-print {
+                display: none;
+            }
+        }
     </style>
 </head>
 
@@ -70,27 +73,27 @@ if ($stmt = $conn->prepare($transaksi_query)) {
             <thead>
                 <tr>
                     <th>Banyaknya</th>
+                    <th>Kategori</th>
                     <th>Barang</th>
-                    <th>Harga</th>
                     <th>Jumlah</th>
                 </tr>
             </thead>
             <tbody>
                 <?php foreach ($items as $item): ?>
-                <tr>
-                    <td><?php echo htmlspecialchars($item['jumlah']) . ' Kg'; ?></td>
-                    <td><?php echo htmlspecialchars($item['jenis_id']); ?></td>
-                    <td><?php echo 'Rp ' . number_format($item['harga'], 0, ',', '.'); ?></td>
-                    <td><?php echo 'Rp ' . number_format($item['jumlah'] * $item['harga'], 0, ',', '.'); ?></td>
-                </tr>
+                    <tr>
+                        <td><?php echo htmlspecialchars($item['jumlah']) . ' Kg'; ?></td>
+                        <td><?php echo htmlspecialchars($item['kategori_name']); ?></td>
+                        <td><?php echo htmlspecialchars($item['jenis_id']); ?></td>
+                        <td><?php echo 'Rp ' . number_format($item['jumlah'] * $item['harga'], 0, ',', '.'); ?></td>
+                    </tr>
                 <?php endforeach; ?>
             </tbody>
             <tfoot>
                 <tr>
                     <th colspan="3" class="text-end">Total</th>
-                    <th><?php echo 'Rp ' . number_format(array_sum(array_map(function($item) {
-                        return $item['jumlah'] * $item['harga'];
-                    }, $items)), 0, ',', '.'); ?></th>
+                    <th><?php echo 'Rp ' . number_format(array_sum(array_map(function ($item) {
+                            return $item['jumlah'] * $item['harga'];
+                        }, $items)), 0, ',', '.'); ?></th>
                 </tr>
             </tfoot>
         </table>
