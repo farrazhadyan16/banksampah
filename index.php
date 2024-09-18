a930063808e2862b711466ffb808190e56650307
Mardhyah <mardhyahfathaniaizzati9d@gmail.com>
2024-09-18 11:04:08 +0700

    hapus

--- index.php
+++ index.php
@@ -120,7 +120,7 @@ $stmt->close();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PMElectric | Dashboard</title>
+    <title>Bank Sampah | Dashboard</title>
 
     <!-- Font Awesome CDN -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
